44987e65fc7bb0faa5e147c0f2d0897c2bae50b3
saurabh <saurabh.saraswat_cs18@gla.ac.in>
2020-03-12 17:34:44 +0530

    complete offer method

--- src/Definition/MyQueue.java
+++ src/Definition/MyQueue.java
@@ -3,9 +3,25 @@ package Definition;
 import Adt.MyQueueAdt;
 
 public class MyQueue<E> implements MyQueueAdt<E> {
+    private Node<E> front;
+    private Node<E> rear ;
+    private int size;
     @Override
     public boolean offer(E item) {
-        return false;
+        if(front==null)
+        {
+            rear= new Node<E>(item);
+            front=rear;
+            size++;
+
+        }
+        else
+        {
+            rear.next=new Node<>(item);
+            rear=rear.next;
+            size++;
+        }
+        return true;
     }
 
     @Override
@@ -26,5 +42,22 @@ public class MyQueue<E> implements MyQueueAdt<E> {
     @Override
     public E element() {
         return null;
+    }
+    private static class  Node<E>
+    {
+        private Node<E> next;
+        private E data;
+
+        public Node(E data) {
+            this.data = data;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public E getData() {
+            return data;
+        }
     }
 }
