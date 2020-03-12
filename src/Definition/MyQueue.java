057e30554e1a4ba0f7f3b6ac00578e5599720ac1
saurabh <saurabh.saraswat_cs18@gla.ac.in>
2020-03-12 17:09:01 +0530

    add MyQueue  Definition class

---
+++ src/Definition/MyQueue.java
@@ -0,0 +1,30 @@
+package Definition;
+
+import Adt.MyQueueAdt;
+
+public class MyQueue implements MyQueueAdt {
+    @Override
+    public boolean offer(Object item) {
+        return false;
+    }
+
+    @Override
+    public Object poll() {
+        return null;
+    }
+
+    @Override
+    public Object remove() {
+        return null;
+    }
+
+    @Override
+    public Object peek() {
+        return null;
+    }
+
+    @Override
+    public Object element() {
+        return null;
+    }
+}
